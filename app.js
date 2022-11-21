bf913a0fac9d087d1acad719413610a2f6922121
Quojo_Steve <quojosteve@gmail.com>
2022-11-21 18:51:08 +0000

    more updates

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const del = document.querySelectorAll('.delete');
+const act = document.querySelectorAll('.activate');
 const display = document.querySelector('.form');
+const display1 = document.querySelector('.form1');
 const remove = document.querySelector('.no');
+const remove1 = document.querySelector('.no1');
 
-del.forEach((btn)=>{
+act.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        display1.classList.remove('remove')
+    })
+});
+del.forEach((btn) => {
     btn.addEventListener('click', () => {
         display.classList.remove('remove')
     })
-})
+});
 remove.addEventListener('click', () => {
     display.classList.add('remove')
+})
+remove1.addEventListener('click', () => {
+    display1.classList.add('remove')
 })
 
 window.addEventListener("resize", function(){
