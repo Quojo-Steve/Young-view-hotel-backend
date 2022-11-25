9d0ec15b9a3b47ba6f0df6c2fc324e70921898a5
Quojo_Steve <quojosteve@gmail.com>
2022-11-25 19:28:11 +0000

    All autjentications working

--- receptionist/url.py
+++ receptionist/url.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
     path('', views.index),
     path('emp_login', views.emp_login),
-    path('emp_change_password', views.empchangepassword),
+    path('emp_dashboard', views.emp_dashboard),
+    path('emp_otp', views.emp_otp),
+    path('emp_register', views.emp_register),
+    path('logout', views.logout),
 ]
--- receptionist/views.py
+++ receptionist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import auth
+from django.contrib.auth.models import auth, User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -15,11 +17,59 @@ def emp_login(request):
         user = auth.authenticate(username = username, password = password)
         
         if user is not None:
-            return redirect('emp_change_password')
+            auth.login(request, user)
+            return redirect('./emp_dashboard')
         else:
-            return redirect('')
+            messages.info(request, 'Invalid Credentials')
+            return redirect('./emp_login')
                 
     return render(request, 'emp_login.html')
 
-def empchangepassword(request):
+
     return render(request, "emp_change_password.html")
+
+def emp_otp(request):
+    if request.method == 'POST':
+        otp = request.POST['otp']
+        if User.objects.filter(first_name = otp).exists():
+            fake = User.objects.filter(first_name = otp)
+            fake.delete()
+            return redirect('./emp_register')
+        else:
+            messages.info(request, 'No such OTP')
+            return redirect('./emp_otp')
+    return render(request, 'emp_otp.html')
+
+def emp_register(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        
+        if password == password1:
+            if User.objects.filter(username = username).exists():
+                messages.info(request, 'Username taken')
+                return redirect('./emp_register')
+            elif User.objects.filter(email = email).exists():
+                messages.info(request, 'Email taken')
+                return redirect('./emp_register')
+            else:
+                User.objects.create_user(username= username, password= password, email=email, last_name= lname, first_name= fname)
+                User.save
+                return redirect('./emp_login')
+        else:
+            messages.info(request, "Passwords don't match")
+            return redirect('./emp_register')
+                
+    return render(request, 'emp_register.html')
+
+@login_required(login_url = './emp_login')
+def emp_dashboard(request):
+    return render(request, 'emp_dashboard.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('./')
